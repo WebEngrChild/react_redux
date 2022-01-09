301809e59956656ed1ff5cafad183cc26f26d7d3
joe-main <qmmplusfact@gmail.com>
2022-01-09 21:09:51 +0900

    mapDispatchToPropsの導入

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import "./App.css";
  * ライブラリ(react-redux)からDL
  */
 import { connect } from "react-redux";
+import Count from './components/Post';
 
-import Count from './components/Count';
+/**
+ * dispatch関数はconect関数からデフォルトで渡される
+ * JSX内イベントに紐づくdispatchが実行
+ * store/index.js/reducerメソッドで条件に合わせて更新処理が走る
+ */
 
-function App({dispatch, count}) {
-  const increase = () => {
-    dispatch({ type: "INCREASE_COUNT" });
-  };
-  const decrease = () => {
-    dispatch({ type: "DECREASE_COUNT" });
-  };
+function App({count,increase, decrease }) {
 
   return (
     <div className="App">
-      <h1>Redux Learn</h1>
+      <h1>Redux アプリケーション</h1>
       <p>Count: {count}</p>
       <button onClick={increase}>Up</button>
       <button onClick={decrease}>Down</button>
@@ -33,6 +32,14 @@ const mapStateToProps = (state) => {
   };
 };
 
+//関数：store
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increase: () => dispatch({ type: "INCREASE_COUNT" }),
+    decrease: () => dispatch({ type: "DECREASE_COUNT" }),
+  };
+};
+
 /**
  * 【流れ】
  * ①<Provider store={store}></Provider>の形でApp.jsにpropsとしてstoreが渡される
@@ -40,4 +47,4 @@ const mapStateToProps = (state) => {
  * ③connect関数の返り値は関数であるためそのままAppを引数にして実行
  * ④Appではcomponent内でpropsで渡されたstateを利用できる
  */
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
