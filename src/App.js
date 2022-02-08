7561a45d6f6ddac2ec0904dcf6ce6a1c6d175595
joe-main <qmmplusfact@gmail.com>
2022-02-08 21:29:00 +0900

    不使用のconect関数の削除

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import "./App.css";
-/**
- * reduxのstoreからstateを取得できるようにするための関数(conect)
- * ライブラリ(react-redux)からDL
- */
-import { connect } from "react-redux";
 import Comment from './components/Comment.js';
 
 function App() {
@@ -16,21 +11,4 @@ function App() {
   );
 }
 
-/**
- * 関数：store内のstateをpropsで扱う
- * ここではcombineReducerを利用していないのでReducer名は不要
- */
-const mapStateToProps = (state) => {
-  return { 
-    comments: state.comments,
-  };
-};
-
-/**
- * 【流れ】
- * ①<Provider store={store}></Provider>の形でApp.jsにpropsとしてstoreが渡される
- * ②connect関数で引数で受けたmapStateToProps関数を利用してstoreのstateをpropsにする
- * ③connect関数の返り値は関数であるためそのままAppを引数にして実行
- * ④Appではcomponent内でpropsで渡されたstateを利用できる
- */
-export default connect(mapStateToProps)(App);
+export default App;
